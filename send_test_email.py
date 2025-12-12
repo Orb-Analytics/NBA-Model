cb50bdf703fae1876797ff468b3ddab61c77af6f
Lpchaitin <lchaiti1@lion.lmu.edu>
2025-12-12 19:52:58 +0000

    Add henry.devlin@icloud.com to predictions email recipients

--- send_test_email.py
+++ send_test_email.py
@@ -39,7 +39,7 @@ def send_test_email():
         return False
     
     # Recipients
-    to_emails = ['lpchaitin@gmail.com', 'eborsook@gmail.com', 'benitesa192@gmail.com']
+    to_emails = ['lpchaitin@gmail.com', 'eborsook@gmail.com', 'benitesa192@gmail.com', 'henry.devlin@icloud.com']
     
     # Create message
     msg = MIMEMultipart('alternative')
