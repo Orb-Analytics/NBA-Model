7b4f6e41bc635c3f9b568ed6edc0dba3b34c556d
Lpchaitin <lchaiti1@lion.lmu.edu>
2026-01-15 18:34:53 +0000

    Use BCC for email recipients to hide recipient list

--- src/predict_and_email_averaged.py
+++ src/predict_and_email_averaged.py
@@ -1043,7 +1043,8 @@ def send_email_html(subject, html_body, predictions=None, yesterday_results=None
     # Create multipart message for embedded images
     msg = MIMEMultipart('related')
     msg['From'] = smtp_username
-    msg['To'] = 'lpchaitin@gmail.com,eborsook@gmail.com,benitesa192@gmail.com,henrykdevlin@gmail.com,alexkalta1@gmail.com,pablozilly@icloud.com'
+    msg['To'] = smtp_username  # Show sender in To field
+    msg['Bcc'] = 'lpchaitin@gmail.com,eborsook@gmail.com,benitesa192@gmail.com,henrykdevlin@gmail.com,alexkalta1@gmail.com,pablozilly@icloud.com'
     msg['Subject'] = subject
     
     # Attach HTML body
@@ -1106,7 +1107,8 @@ def send_email(subject, body):
     # Create message
     msg = MIMEMultipart()
     msg['From'] = smtp_username
-    msg['To'] = 'lpchaitin@gmail.com,eborsook@gmail.com,benitesa192@gmail.com,henrykdevlin@gmail.com,alexkalta1@gmail.com,pablozilly@icloud.com,blake1809@live.com'
+    msg['To'] = smtp_username  # Show sender in To field
+    msg['Bcc'] = 'lpchaitin@gmail.com,eborsook@gmail.com,benitesa192@gmail.com,henrykdevlin@gmail.com,alexkalta1@gmail.com,pablozilly@icloud.com,blake1809@live.com'
     msg['Subject'] = subject
     
     msg.attach(MIMEText(body, 'plain'))
